14e21712580b25727c2e7e14fe11ae1324372a66
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-24 19:44:10 +0900

    add output

---
+++ modules/nw/output.tf
@@ -0,0 +1,40 @@
+# ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+# ║ PCA EC2 SSL Stack - Terraform output.tf output                                                                                                   ║
+# ╠═════════════════════════════╤════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣
+# ║ subnetid_for_bastion        │ Subnet ID for Bastion EC2.                                                                                         ║
+# ║ subnetid_for_linux          │ Subnet ID for Linux EC2.                                                                                           ║
+# ║ subnetid_for_windows        │ Subnet ID for Windows EC2.                                                                                         ║
+# ║ sgid_for_bastion            │ SecurityGroup ID for Bastion EC2.                                                                                  ║
+# ║ sgid_for_linux              │ SecurityGroup ID for Linux EC2.                                                                                    ║
+# ║ sgid_for_windows            │ SecurityGroup ID for Windows EC2.                                                                                  ║
+# ╚═════════════════════════════╧════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+
+output "subnetid_for_bastion" {
+  description = "Subnet ID for Bastion EC2."
+  value       = aws_subnet.subnet["public-subnet-a"].id
+}
+
+output "subnetid_for_linux" {
+  description = "Subnet ID for Linux EC2."
+  value       = aws_subnet.subnet["private-subnet-a"].id
+}
+
+output "subnetid_for_windows" {
+  description = "Subnet ID for Windows EC2."
+  value       = aws_subnet.subnet["private-subnet-c"].id
+}
+
+output "sgid_for_bastion" {
+  description = "SecurityGroup ID for Bastion EC2."
+  value       = aws_security_group.bastion_ec2_sg.id
+}
+
+output "sgid_for_linux" {
+  description = "SecurityGroup ID for Linux EC2."
+  value       = aws_security_group.linux_ec2_sg.id
+}
+
+output "sgid_for_windows" {
+  description = "SecurityGroup ID for Windows EC2."
+  value       = aws_security_group.windows_ec2_sg.id
+}
