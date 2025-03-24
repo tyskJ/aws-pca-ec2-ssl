1661123e409ebe3e7156c22ce561a38ba4c0afe0
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-24 23:55:10 +0900

    add output vpc id

--- modules/nw/output.tf
+++ modules/nw/output.tf
@@ -1,6 +1,7 @@
 # ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
 # ║ PCA EC2 SSL Stack - Terraform output.tf output                                                                                                   ║
 # ╠═════════════════════════════╤════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣
+# ║ vpc_id                      │ VPC ID.                                                                                                            ║
 # ║ subnetid_for_bastion        │ Subnet ID for Bastion EC2.                                                                                         ║
 # ║ subnetid_for_linux          │ Subnet ID for Linux EC2.                                                                                           ║
 # ║ subnetid_for_windows        │ Subnet ID for Windows EC2.                                                                                         ║
@@ -8,6 +9,11 @@
 # ║ sgid_for_linux              │ SecurityGroup ID for Linux EC2.                                                                                    ║
 # ║ sgid_for_windows            │ SecurityGroup ID for Windows EC2.                                                                                  ║
 # ╚═════════════════════════════╧════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+
+output "vpc_id" {
+  description = "VPC ID."
+  value       = aws_vpc.vpc.id
+}
 
 output "subnetid_for_bastion" {
   description = "Subnet ID for Bastion EC2."
